d9e7f3497d6df6469d251e90914e7c77ff5d2e31
Zaouiadnane <95150700+Adnanezaoui@users.noreply.github.com>
2026-02-12 19:03:50 +0400

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,45 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 let players = {};
+let adminId = null;
+let gameStarted = false;
+let currentRound = 0;
 
 io.on("connection", (socket) => {
 
-  socket.on("joinGame", (name) => {
-    players[socket.id] = { name, score: 0 };
+  socket.on("login", ({ name, password }) => {
+
+    if (password !== "POLO FAMILY") {
+      socket.emit("loginError");
+      return;
+    }
+
+    if (!adminId) {
+      adminId = socket.id; // أول لاعب هو الأدمن
+    }
+
+    players[socket.id] = {
+      name,
+      score: 0
+    };
+
     io.emit("updatePlayers", players);
+    io.emit("adminUpdate", adminId);
+  });
+
+  socket.on("startGame", () => {
+    if (socket.id === adminId) {
+      gameStarted = true;
+      currentRound = 1;
+      io.emit("countdown", 10);
+    }
+  });
+
+  socket.on("nextRound", () => {
+    if (socket.id === adminId) {
+      currentRound++;
+      io.emit("countdown", 10);
+    }
   });
 
   socket.on("addPoint", () => {
@@ -24,8 +57,17 @@ io.on("connection", (socket) => {
     }
   });
 
+  socket.on("stopGame", () => {
+    if (socket.id === adminId) {
+      gameStarted = false;
+      currentRound = 0;
+      io.emit("gameStopped");
+    }
+  });
+
   socket.on("disconnect", () => {
     delete players[socket.id];
+    if (socket.id === adminId) adminId = null;
     io.emit("updatePlayers", players);
   });
 
